f78f1145bfacfb438a7a939cd959a9eda6f299aa
Kin <kin.tang@gmail.com>
2015-12-19 17:16:16 +0800

    reset

--- kinkincup/js/connections/production-alternative.js
+++
@@ -1,3 +0,0 @@
-var ip = "10.20.225.135";
-var login = "ZONE1-SCBNET\\1400364";
-var port = "8899";
--- kinkincup/js/connections/uat.js
+++
@@ -1,3 +0,0 @@
-var ip = "10.20.40.58";
-var login = "ZONE1-SCBNET\\1400364";
-var port = "7788";
